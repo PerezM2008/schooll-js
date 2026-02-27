8aa865565ec570909320d269477cef5ddce251d7
MATHEUS GABRIEL PEREZ DOS SANTOS <25132972@CFP127ALUNO.LOCAL>
2026-02-27 16:58:25 -0300

    Loop alunos

--- index.js
+++ index.js
@@ -6,44 +6,64 @@
  * *********************************************************/
 
 //Função para buscar os dados da API
+
 async function buscarDados(){
 
-    const containerAlunos = document.getElementById('cardAlunos')
-
     try{
+
         const URL = await fetch('https://lion-school-phbo.onrender.com/alunos')
         const dadosJSON = await URL.json();
 
+        const containerAlunos = document.getElementById('cardAlunos')
 
-        console.log(dadosJSON)
-        
-         dadosJSON.forEach( item => {
+        dadosJSON.forEach( item => {
 
             const card = document.createElement('div')
             card.classList.add('card')
 
 
             const img = document.createElement('img')
+            img.classList.add('img')
             img.src = item.foto;
 
             const nome = document.createElement('p')
             nome.textContent = item.nome;
 
-
-
             card.appendChild(img);
             card.appendChild(nome);
             containerAlunos.appendChild(card)
+
+            return containerAlunos
             
          });
+
 
          console.log(dadosJSON)
 
     } 
     catch (error) {
+        console.log(error)
         return false
     }
     
 };
+
+async function buttonEvent() {
+    try{
+
+        var buttonDev = document.getElementById('devButton')
+        
+        var buttonDev = document.getElementById('redesButton')
+
+
+
+
+
+    } catch {
+
+    }
+ };
+
 buscarDados();
+
 
